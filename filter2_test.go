a0d60b4d4ab63864562affe9a0f75352cda45ea4
金 <>
2022-05-12 18:16:22 +0800

    二维卷积通过协程优化

--- filter2_test.go
+++ filter2_test.go
@@ -1,23 +1,27 @@
 package go_math
 
 import (
+	"math/rand"
 	"testing"
 )
 
-var xss = [][]int{
-	{1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1},
-}
-var hss = [][]int{
-	{1, 1, 1},
-	{1, 1, 1},
-	{1, 1, 1},
+func gens[T ~int | ~float32 | ~float64](m, n int) [][]T {
+	sss := make([][]T, m)
+	for i := range sss {
+		sss[i] = make([]T, n)
+		for j := range sss[i] {
+			sss[i][j] = T(rand.Float64() * 10)
+		}
+	}
+	return sss
 }
 
+var xss = gens[int](48000, 10)
+var hss = gens[int](9, 1)
+
 func Test_filter2full(t *testing.T) {
+	t.Log(xss)
+	t.Log(hss)
 	t.Log(filter2full[int](hss, xss))
 }
 
@@ -28,6 +32,8 @@ func Benchmark_filter2full(b *testing.B) {
 }
 
 func Test_filter2same(t *testing.T) {
+	t.Log(xss)
+	t.Log(hss)
 	t.Log(filter2same[int](hss, xss))
 }
 
@@ -38,6 +44,8 @@ func Benchmark_filter2same(b *testing.B) {
 }
 
 func Test_filter2valid(t *testing.T) {
+	t.Log(xss)
+	t.Log(hss)
 	t.Log(filter2valid[int](hss, xss))
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kcgp007/go-math
 
 go 1.18
 
-require golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f
+require (
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f
+)
